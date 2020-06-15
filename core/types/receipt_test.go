2354157e948bea2c675be31f2424b3486fc5cc17
Misha <slyvkamm@gmail.com>
2020-06-15 11:50:47 +0300

    some tests

--- core/types/receipt_test.go
+++ core/types/receipt_test.go
@@ -50,7 +50,7 @@ func TestLegacyReceiptDecoding(t *testing.T) {
 
 	tx := NewTransaction(1, common.HexToAddress("0x1"), big.NewInt(1), 1, big.NewInt(1), nil)
 	receipt := &Receipt{
-		Status:            ReceiptStatusFailed,
+		Status:               ReceiptStatusFailed,
 		CumulativeEnergyUsed: 1,
 		Logs: []*Log{
 			{
@@ -66,7 +66,7 @@ func TestLegacyReceiptDecoding(t *testing.T) {
 		},
 		TxHash:          tx.Hash(),
 		ContractAddress: common.BytesToAddress([]byte{0x01, 0x11, 0x11}),
-		EnergyUsed:         111111,
+		EnergyUsed:      111111,
 	}
 	receipt.Bloom = CreateBloom(Receipts{receipt})
 
@@ -110,9 +110,9 @@ func TestLegacyReceiptDecoding(t *testing.T) {
 
 func encodeAsStoredReceiptRLP(want *Receipt) ([]byte, error) {
 	stored := &storedReceiptRLP{
-		PostStateOrStatus: want.statusEncoding(),
+		PostStateOrStatus:    want.statusEncoding(),
 		CumulativeEnergyUsed: want.CumulativeEnergyUsed,
-		Logs:              make([]*LogForStorage, len(want.Logs)),
+		Logs:                 make([]*LogForStorage, len(want.Logs)),
 	}
 	for i, log := range want.Logs {
 		stored.Logs[i] = (*LogForStorage)(log)
@@ -122,11 +122,11 @@ func encodeAsStoredReceiptRLP(want *Receipt) ([]byte, error) {
 
 func encodeAsV4StoredReceiptRLP(want *Receipt) ([]byte, error) {
 	stored := &v4StoredReceiptRLP{
-		PostStateOrStatus: want.statusEncoding(),
+		PostStateOrStatus:    want.statusEncoding(),
 		CumulativeEnergyUsed: want.CumulativeEnergyUsed,
-		TxHash:            want.TxHash,
-		ContractAddress:   want.ContractAddress,
-		Logs:              make([]*LogForStorage, len(want.Logs)),
+		TxHash:               want.TxHash,
+		ContractAddress:      want.ContractAddress,
+		Logs:                 make([]*LogForStorage, len(want.Logs)),
 		EnergyUsed:           want.EnergyUsed,
 	}
 	for i, log := range want.Logs {
@@ -137,12 +137,12 @@ func encodeAsV4StoredReceiptRLP(want *Receipt) ([]byte, error) {
 
 func encodeAsV3StoredReceiptRLP(want *Receipt) ([]byte, error) {
 	stored := &v3StoredReceiptRLP{
-		PostStateOrStatus: want.statusEncoding(),
+		PostStateOrStatus:    want.statusEncoding(),
 		CumulativeEnergyUsed: want.CumulativeEnergyUsed,
-		Bloom:             want.Bloom,
-		TxHash:            want.TxHash,
-		ContractAddress:   want.ContractAddress,
-		Logs:              make([]*LogForStorage, len(want.Logs)),
+		Bloom:                want.Bloom,
+		TxHash:               want.TxHash,
+		ContractAddress:      want.ContractAddress,
+		Logs:                 make([]*LogForStorage, len(want.Logs)),
 		EnergyUsed:           want.EnergyUsed,
 	}
 	for i, log := range want.Logs {
@@ -161,7 +161,7 @@ func TestDeriveFields(t *testing.T) {
 	// Create the corresponding receipts
 	receipts := Receipts{
 		&Receipt{
-			Status:            ReceiptStatusFailed,
+			Status:               ReceiptStatusFailed,
 			CumulativeEnergyUsed: 1,
 			Logs: []*Log{
 				{Address: common.BytesToAddress([]byte{0x11})},
@@ -169,10 +169,10 @@ func TestDeriveFields(t *testing.T) {
 			},
 			TxHash:          txs[0].Hash(),
 			ContractAddress: common.BytesToAddress([]byte{0x01, 0x11, 0x11}),
-			EnergyUsed:         1,
+			EnergyUsed:      1,
 		},
 		&Receipt{
-			PostState:         common.Hash{2}.Bytes(),
+			PostState:            common.Hash{2}.Bytes(),
 			CumulativeEnergyUsed: 3,
 			Logs: []*Log{
 				{Address: common.BytesToAddress([]byte{0x22})},
@@ -180,7 +180,7 @@ func TestDeriveFields(t *testing.T) {
 			},
 			TxHash:          txs[1].Hash(),
 			ContractAddress: common.BytesToAddress([]byte{0x02, 0x22, 0x22}),
-			EnergyUsed:         2,
+			EnergyUsed:      2,
 		},
 	}
 	// Clear all the computed fields and re-derive them
@@ -192,7 +192,7 @@ func TestDeriveFields(t *testing.T) {
 		t.Fatalf("DeriveFields(...) = %v, want <nil>", err)
 	}
 	// Iterate over all the computed fields and check that they're correct
-	signer := MakeSigner()
+	signer := MakeSigner(params.TestChainConfig.ChainID)
 
 	logIndex := uint(0)
 	for i := range receipts {
--- xce/fetcher/tx_fetcher_test.go
+++ xce/fetcher/tx_fetcher_test.go
@@ -32,10 +32,10 @@ import (
 var (
 	// testTxs is a set of transactions to use during testing that have meaninful hashes.
 	testTxs = []*types.Transaction{
-		types.NewTransaction(15352856648520921629, common.Address{0xbb}, new(big.Int), 0, new(big.Int), nil),
-		types.NewTransaction(3916589616287113937, common.Address{0x86}, new(big.Int), 0, new(big.Int), nil),
 		types.NewTransaction(5577006791947779410, common.Address{0x0f}, new(big.Int), 0, new(big.Int), nil),
+		types.NewTransaction(15352856648520921629, common.Address{0xbb}, new(big.Int), 0, new(big.Int), nil),
 		types.NewTransaction(9828766684487745566, common.Address{0xac}, new(big.Int), 0, new(big.Int), nil),
+		types.NewTransaction(3916589616287113937, common.Address{0x86}, new(big.Int), 0, new(big.Int), nil),
 	}
 	// testTxsHashes is the hashes of the test transactions above
 	testTxsHashes = []common.Hash{testTxs[0].Hash(), testTxs[1].Hash(), testTxs[2].Hash(), testTxs[3].Hash()}
@@ -226,7 +226,7 @@ func TestTransactionFetcherSkipWaiting(t *testing.T) {
 }
 
 // Tests that only a single transaction request gets scheduled to a peer
-// and subsequent announces block or get allotted to someone else. 
+// and subsequent announces block or get allotted to someone else.
 func TestTransactionFetcherSingletonRequesting(t *testing.T) {
 	testTransactionFetcherParallel(t, txFetcherTest{
 		init: func() *TxFetcher {
