ce93c5b153e8131e07e9b15e8c7211cd6ad93c7d
Misha <slyvkamm@gmail.com>
2020-07-03 20:37:16 +0300

    refactoring

--- accounts/keystore/passphrase_test.go
+++ accounts/keystore/passphrase_test.go
@@ -35,8 +35,7 @@ func TestKeyEncryptDecrypt(t *testing.T) {
 		t.Fatal(err)
 	}
 	password := ""
-	address := common.HexToAddress("798682fac39cfe662bb6d451827fd16b4a1a7172")
-
+	address := common.HexToAddress("37798682fac39cfe662bb6d451827fd16b4a1a7172")
 	// Do a few rounds of decryption and encryption
 	for i := 0; i < 3; i++ {
 		// Try a bad password first
