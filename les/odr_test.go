795d5b17f2b6d53fbec0259f5912edd470fbabc4
Misha <slyvkamm@gmail.com>
2020-07-09 17:16:36 +0300

    increase timeout time in testOdr

--- les/odr_test.go
+++ les/odr_test.go
@@ -238,7 +238,7 @@ func testOdr(t *testing.T, protocol int, expFail uint64, checkCached bool, fn od
 	// still expect all retrievals to pass, now data should be cached locally
 	if checkCached {
 		client.handler.backend.peers.unregister(client.peer.speer.id)
-		time.Sleep(time.Millisecond * 10) // ensure that all peerSetNotify callbacks are executed
+		time.Sleep(time.Second * 1) // ensure that all peerSetNotify callbacks are executed
 		test(5)
 	}
 }
