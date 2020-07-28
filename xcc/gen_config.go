4e8ed248b6e456c4406bffeb4073153d3e03941b
error2215 <vsifyz20002000!>
2020-07-28 14:01:12 +0300

    turn off dns discovery by default

--- xcc/gen_config.go
+++ xcc/gen_config.go
@@ -22,6 +22,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 		NetworkId               uint64
 		SyncMode                downloader.SyncMode
 		DiscoveryURLs           []string
+		UseDNSDiscovery         bool
 		NtpServer               string
 		NoPruning               bool
 		NoPrefetch              bool
@@ -40,6 +41,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 		TrieCleanCache          int
 		TrieDirtyCache          int
 		TrieTimeout             time.Duration
+		SnapshotCache           int
 		Miner                   miner.Config
 		Cryptore                cryptore.Config
 		TxPool                  core.TxPoolConfig
@@ -57,6 +59,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 	enc.NetworkId = c.NetworkId
 	enc.SyncMode = c.SyncMode
 	enc.DiscoveryURLs = c.DiscoveryURLs
+	enc.UseDNSDiscovery = c.UseDNSDiscovery
 	enc.NtpServer = c.NtpServer
 	enc.NoPruning = c.NoPruning
 	enc.NoPrefetch = c.NoPrefetch
@@ -75,6 +78,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 	enc.TrieCleanCache = c.TrieCleanCache
 	enc.TrieDirtyCache = c.TrieDirtyCache
 	enc.TrieTimeout = c.TrieTimeout
+	enc.SnapshotCache = c.SnapshotCache
 	enc.Miner = c.Miner
 	enc.Cryptore = c.Cryptore
 	enc.TxPool = c.TxPool
@@ -96,6 +100,7 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 		NetworkId               *uint64
 		SyncMode                *downloader.SyncMode
 		DiscoveryURLs           []string
+		UseDNSDiscovery         *bool
 		NtpServer               *string
 		NoPruning               *bool
 		NoPrefetch              *bool
@@ -114,6 +119,7 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 		TrieCleanCache          *int
 		TrieDirtyCache          *int
 		TrieTimeout             *time.Duration
+		SnapshotCache           *int
 		Miner                   *miner.Config
 		Cryptore                *cryptore.Config
 		TxPool                  *core.TxPoolConfig
@@ -141,6 +147,9 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 	}
 	if dec.DiscoveryURLs != nil {
 		c.DiscoveryURLs = dec.DiscoveryURLs
+	}
+	if dec.UseDNSDiscovery != nil {
+		c.UseDNSDiscovery = *dec.UseDNSDiscovery
 	}
 	if dec.NtpServer != nil {
 		c.NtpServer = *dec.NtpServer
@@ -195,6 +204,9 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 	}
 	if dec.TrieTimeout != nil {
 		c.TrieTimeout = *dec.TrieTimeout
+	}
+	if dec.SnapshotCache != nil {
+		c.SnapshotCache = *dec.SnapshotCache
 	}
 	if dec.Miner != nil {
 		c.Miner = *dec.Miner
