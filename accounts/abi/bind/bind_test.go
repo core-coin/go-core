e747ca5bc341ac8461ec5f4b2aabccca560818b2
error2215 <slyvkamm@gmail.com>
2020-10-20 13:45:47 +0300

    test fix

--- accounts/abi/bind/bind_test.go
+++ accounts/abi/bind/bind_test.go
@@ -1655,7 +1655,7 @@ func TestGolangBindings(t *testing.T) {
 		t.Fatalf("failed to convert binding test to modules: %v\n%s", err, out)
 	}
 	pwd, _ := os.Getwd()
-	replacer := exec.Command(gocmd, "mod", "edit", "-replace", "github.com/core-coin/go-core="+filepath.Join(pwd, "..", "..", "..")) // Repo root
+	replacer := exec.Command(gocmd, "mod", "edit", "-replace", "github.com/core-coin/go-core/v2="+filepath.Join(pwd, "..", "..", "..")) // Repo root
 	replacer.Dir = pkg
 	if out, err := replacer.CombinedOutput(); err != nil {
 		t.Fatalf("failed to replace binding test dependency to current source tree: %v\n%s", err, out)
