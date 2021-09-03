d8a1718732b5d655e46c264b89ecad5cff860005
error2215 <slyvkamm@gmail.com>
2021-09-03 15:49:10 +0300

    increase timeouts for p2p discover

--- p2p/discover/v5_udp.go
+++ p2p/discover/v5_udp.go
@@ -42,7 +42,7 @@ const (
 	totalNodesResponseLimit = 5  // applies in waitForNodes
 	nodesResponseItemLimit  = 3  // applies in sendNodes
 
-	respTimeoutV5 = 3000 * time.Millisecond
+	respTimeoutV5 = 5000 * time.Millisecond
 )
 
 // codecV5 is implemented by wireCodec (and testCodec).
