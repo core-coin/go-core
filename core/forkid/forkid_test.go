03cdfa950ce9eca5f2e26eb42df3495d8c151a9d
error2215 <slyvkamm@gmail.com>
2021-10-27 15:56:55 +0300

    fix test

--- core/forkid/forkid_test.go
+++ core/forkid/forkid_test.go
@@ -59,7 +59,7 @@ func TestCreation(t *testing.T) {
 			params.KolibaChainConfig,
 			params.KolibaGenesisHash,
 			[]testcase{
-				{1561651, ID{Hash: checksumToBytes(0xdcf1d320), Next: 0}},
+				{1561651, ID{Hash: checksumToBytes(0xc8a79efc), Next: 0}},
 			},
 		},
 	}
