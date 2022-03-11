8da260b0876eb8ba46aa4f91e9d502700c25b580
error2215 <slyvkamm@gmail.com>
2022-03-11 11:23:11 +0200

    warn of bad bootnode

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -766,7 +766,7 @@ func setBootstrapNodes(ctx *cli.Context, cfg *p2p.Config) {
 		if url != "" {
 			node, err := enode.Parse(enode.ValidSchemes, url)
 			if err != nil {
-				log.Crit("Bootstrap URL invalid", "enode", url, "err", err)
+				log.Warn("Bootstrap URL invalid, skipping it", "enode", url, "err", err)
 				continue
 			}
 			cfg.BootstrapNodes = append(cfg.BootstrapNodes, node)
