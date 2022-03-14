4ff19b805080644df306c34dee4148850f9c4055
error2215 <slyvkamm@gmail.com>
2022-03-14 17:02:23 +0100

    iot support

---
+++ core/iot/iot.go
@@ -0,0 +1,47 @@
+package iot
+
+import "github.com/stianeikeland/go-rpio/v4"
+
+const (
+	pin = 1
+)
+
+func EnablePin() error {
+	err := rpio.Open()
+	if err != nil {
+		return err
+	}
+	pin := rpio.Pin(pin)
+	pin.Input() // Input mode
+
+	// Read status
+	res := pin.Read()
+
+	// Switch on
+	pin.Output()
+	if res == rpio.High {
+		pin.Low()
+	} else {
+		pin.High()
+	}
+
+	return nil
+}
+
+func DisablePin() error {
+	pin := rpio.Pin(pin)
+	pin.Input() // Input mode
+
+	// Read status
+	res := pin.Read()
+
+	// Switch on
+	pin.Output()
+	if res == rpio.High {
+		pin.Low()
+	} else {
+		pin.High()
+	}
+
+	return rpio.Close()
+}
