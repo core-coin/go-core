ea7e889f3af6da7fd2ca1706d67c8922c0d50edc
error2215 <slyvkamm@gmail.com>
2022-04-17 15:05:06 +0200

    tests

--- core/genesis_test.go
+++ core/genesis_test.go
@@ -17,6 +17,7 @@
 package core
 
 import (
+	"fmt"
 	"math/big"
 	"reflect"
 	"testing"
@@ -31,7 +32,10 @@ import (
 )
 
 func TestDefaultGenesisBlock(t *testing.T) {
-
+	data := decodePrealloc(mainnetAllocData)
+	for a, k := range data {
+		fmt.Println(a.Hex(), " : ", k.Balance)
+	}
 	block := DefaultGenesisBlock().ToBlock(nil)
 	if block.Hash() != params.MainnetGenesisHash {
 		t.Errorf("wrong mainnet genesis hash, got %v, want %v", block.Hash(), params.MainnetGenesisHash)
