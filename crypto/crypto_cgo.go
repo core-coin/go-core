11111a4fd34607d6d10cb9ea17e0c63843f594e3
error2215 <misha.slyvka@wallit.eu>
2023-06-05 12:30:50 +0300

    Use ed448 gocore library instead of go-golidlocks on mobile devices + version typo fix (#450)
    * new build constraints
    
    * fix version typo

--- crypto/crypto_cgo.go
+++ crypto/crypto_cgo.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-core library. If not, see <http://www.gnu.org/licenses/>.
 
-//go:build !nacl && !js && cgo
+//go:build !nacl && !js && cgo && !android && !ios && !mobile
 
 package crypto
 
